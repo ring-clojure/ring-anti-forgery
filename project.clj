40ddbc79ecbaa7d8ae56021a82e42a659bc18f1c
rome-user <114131048+rome-user@users.noreply.github.com>
2023-05-22 00:32:12 -0700

    Update minimum Clojure version to 1.7.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ring-clojure/ring-anti-forgery"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [crypto-random "1.2.0"]
                  [crypto-equality "1.0.0"]
                  [hiccup "1.0.5"]]
@@ -12,10 +12,10 @@
   {:output-path "codox"
    :project     {:name "Ring Anti-Forgery"}
    :source-uri  "http://github.com/ring-clojure/ring-anti-forgery/blob/{version}/{filepath}#L{line}"}
-  :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10:+1.11" "test"]}
   :profiles
-  {:dev {:dependencies [[ring/ring-mock "0.3.2"]]}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}})
+  {:dev  {:dependencies [[ring/ring-mock "0.3.2"]]}
+   :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
+   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
+   :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}})
