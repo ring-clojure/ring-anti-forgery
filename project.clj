22fa4d12ac0ed27c5e1b43f0f506ae780a0b594a
rome-user <114131048+rome-user@users.noreply.github.com>
2023-05-22 00:52:15 -0700

    Update outdated dependencies and plugins

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [crypto-random "1.2.0"]
-                 [crypto-equality "1.0.0"]
+                 [crypto-random "1.2.1"]
+                 [crypto-equality "1.0.1"]
                  [hiccup "1.0.5"]]
-  :plugins [[lein-codox "0.10.3"]]
+  :plugins [[lein-codox "0.10.8"]]
   :codox
   {:output-path "codox"
    :project     {:name "Ring Anti-Forgery"}
    :source-uri  "http://github.com/ring-clojure/ring-anti-forgery/blob/{version}/{filepath}#L{line}"}
   :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10:+1.11" "test"]}
   :profiles
-  {:dev  {:dependencies [[ring/ring-mock "0.3.2"]]}
+  {:dev  {:dependencies [[ring/ring-mock "0.4.0"]]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
