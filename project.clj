726298b7b661c62f83e98c529df82fdfcc47ceb1
James Reeves <jreeves@weavejester.com>
2024-04-26 19:56:32 +0100

    Update minimum Clojure version to 1.9.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ring-clojure/ring-anti-forgery"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [crypto-random "1.2.1"]
                  [crypto-equality "1.0.1"]
                  [hiccup "1.0.5"]]
@@ -12,10 +12,9 @@
   {:output-path "codox"
    :project     {:name "Ring Anti-Forgery"}
    :source-uri  "http://github.com/ring-clojure/ring-anti-forgery/blob/{version}/{filepath}#L{line}"}
-  :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10:+1.11" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.10:+1.11:+1.12" "test"]}
   :profiles
   {:dev  {:dependencies [[ring/ring-mock "0.4.0"]]}
-   :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
-   :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}})
+   :1.11 {:dependencies [[org.clojure/clojure "1.11.3"]]}
+   :1.12 {:dependencies [[org.clojure/clojure "1.12.0-alpha9"]]}})
