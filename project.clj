4e226df604dca13d5c93be4d4900367a64f5963e
Mathäus Sander <mathaeus_sander@ochsnersport.ch>
2024-06-21 12:19:12 +0200

    Remove dependency on crypto-random
    Remove crypto-random dependency and transitive dependency commons-codec
    in order to make this library compatible with Babashka.

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [crypto-random "1.2.1"]
                  [crypto-equality "1.0.1"]
                  [hiccup "1.0.5"]]
   :plugins [[lein-codox "0.10.8"]]
