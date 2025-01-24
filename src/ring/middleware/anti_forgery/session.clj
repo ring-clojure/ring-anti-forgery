134c95d1ccf441a494990a163deff3dc7809a78c
James Reeves <jreeves@weavejester.com>
2025-01-24 04:55:01 +0000

    Remove dependency on crypto-random
    Remove crypto-random dependency and transitive dependency commons-codec
    in order to make this library compatible with Babashka.
    
    Co-authored-by: Mathäus Sander <mathaeus_sander@ochsnersport.ch>

--- src/ring/middleware/anti_forgery/session.clj
+++ src/ring/middleware/anti_forgery/session.clj
@@ -1,8 +1,14 @@
 (ns ring.middleware.anti-forgery.session
   "Contains the synchronizer token (or session) strategy."
   (:require [ring.middleware.anti-forgery.strategy :as strategy]
-            [crypto.equality :as crypto]
-            [crypto.random :as random]))
+            [crypto.equality :as crypto]))
+
+(defn- random-base64 [buffer-size]
+  (let [random (java.security.SecureRandom.)
+        base64 (.withoutPadding (java.util.Base64/getEncoder))
+        buffer (byte-array buffer-size)]
+    (.nextBytes random buffer)
+    (.encodeToString base64 buffer)))
 
 (defn- session-token [request]
   (get-in request [:session :ring.middleware.anti-forgery/anti-forgery-token]))
@@ -11,7 +17,7 @@
   strategy/Strategy
   (get-token [this request]
     (or (session-token request)
-        (random/base64 60)))
+        (random-base64 60)))
 
   (valid-token? [_ request token]
     (when-let [stored-token (session-token request)]
